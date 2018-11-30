50111c83202cce53512baa982faa650fe4095c12
Daniel Idris <daniel.idris@bouvet.no>
2018-11-30 22:04:30 +0100

    Initiell commit

---
+++ server/src/main/java/no/hydra/julekalender/JulekalenderApplication.java
@@ -0,0 +1,12 @@
+package no.hydra.julekalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulekalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulekalenderApplication.class, args);
+	}
+}
---
+++ server/src/test/java/no/hydra/julekalender/JulekalenderApplicationTests.java
@@ -0,0 +1,16 @@
+package no.hydra.julekalender;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JulekalenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
